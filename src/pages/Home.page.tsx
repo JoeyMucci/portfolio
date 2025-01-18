79dc490f969519bc315a8edb57cf742908df30ea
JoeyMucci <jmucci314@gmail.com>
2025-01-18 16:06:49 -0500

    added chesscom rapid rating

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,6 +1,35 @@
 import { SideNavBar } from '@/components/SideNavBar/SideNavBar';
 // import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Text, Stack } from '@mantine/core';
+import { Stack } from '@mantine/core';
+import { useState } from 'react';
+import axios from 'axios';
+
+function Stats() {
+    const pathold : string = `https://leetcode.com/graphql?query=query%20%7B%20userContestRanking
+    // (username%3A%20%22jmucc314%22)%20%7B%20rating%20%7D%20%7D`
+
+    const path : string = `https://api.chess.com/pub/player/joeypat11/stats`
+
+    const [chessRating, setChessRating] = useState<number | string>("Loading..." )
+
+    axios.get(path).then(
+      (response) => {
+          setChessRating(response.data.chess_rapid.last.rating)
+      },
+      (_) => {
+        setChessRating('Unavailable')
+      }
+    )
+    
+
+    return (
+      <p>{chessRating}</p>
+    )
+
+    /*
+    https://leetcode.com/graphql?query=query%20%7B%20userContestRanking(username%3A%20%22jmucc314%22)%20%7B%20rating%20%7D%20%7D
+    */
+}
 
 export function HomePage() {
   // const { colorScheme } = useMantineColorScheme();
@@ -23,248 +52,9 @@ export function HomePage() {
 
   return (
     <>
-      <SideNavBar />
+      <SideNavBar />     
       <Stack align="center">
-        <Text>
-          QWERTYUIOPLKJHGFDSAZXCVBNM
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
-        <Text>
-          qwertyuioplkjhgfdsxcvbnm
-        </Text>
+        <Stats />
       </Stack>
     </>
   )
