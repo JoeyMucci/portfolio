623c3b5667a8f8753d6b0f9c9b166ae62568fcbb
JoeyMucci <jmucci314@gmail.com>
2025-02-14 20:15:42 -0500

    added 404 to build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -9,4 +14,12 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '404.html'),
+      },
+    },
+  }
 });
